b752b43bce1370d30640eb8ca4572b9bca49c370
Karter Moua <kartermoua151@gmail.com>
2024-10-10 10:26:43 -0600

    assignment4.1 commiting some stuff

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,39 +15,53 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return "Hello, " +  name  + '!';
 }
+console.log(helloName('Karter'));
 // Remember to call the function to test
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
-}
-
+  return firstNumber + secondNumber;
+ }
+addNumbers(5,4)
+console.log('the total of firstNumber&secondNumber is:', addNumbers(5,4))
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
+function multiplyThree(a,b,c) {
+  return a * b * c;
 
 }
-
+multiplyThree(2,5,7);
+console.log('Multiplied 3 answer:', multiplyThree(2,5,7));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  else{
+
+  }
+  return false;
 }
+isPositive(7);
+console.log('is this number 7 positive?',isPositive(7))
+isPositive(-4)
+console.log('is the number -4 positive?', isPositive(-4))
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast(array) {
 
+function getLast(array) {
+ 
 }
 
 // 7. Function to find a value in an array. Return true if the 
